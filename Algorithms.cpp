9941005987f467dd3d282ca75a6549f10c20cf74
superma30 <126937712+superma30@users.noreply.github.com>
2026-04-09 20:33:32 +0200

    Started working on the program

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,10 +1,20 @@
-
-
 static class Algorithm {
 public:
+    enum AlgorithmNames {
+        _BubbleSort = 1,
+      };
+    static int CallAlgorithmByName(AlgorithmNames name, int *array, int size) {
+        switch (name) {
+            case 1:
+                return BubbleSort(array, size);
+        }
+        return -1;
+    }
     static int BubbleSort(int *array, int size) {
+        int iterations = 0;
         for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size - i - 1; j++) {
+                iterations++;
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
                     array[j] = array[j + 1];
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "matplotlibcpp.h"
 
 /* mathplotlib-cpp
  * stats:
@@ -14,7 +15,51 @@
  *     stats
  */
 
-int main() {
+using namespace std;
 
-    return 0;
+void SingleMode();
+void CompareMode();
+
+void printMainMenu() {
+    cout << "Welcome to Algorithm Analizer!\n------------------------------";
+    cout << "What do you want to do?";
+    cout << "1. Single - Get statistics and graphs for a single algorithm"<<endl;
+    cout << "2. Compare - Compare statistics of two algorithms"<<endl;
+    cout << "3. Exit"<<endl;
+}
+
+int main() {
+    while (true) {
+        printMainMenu();
+        string input;
+        bool validInput = false;
+        do {
+            cin >> input;
+            if (input.length() == 1) {
+                if (input == "1" || input == "2" || input == "3") {
+                    validInput = true;
+                }
+            }
+        }while (!validInput);
+        if (input == "1") {
+            SingleMode();
+        }
+        else if (input == "2") {
+            CompareMode();
+        }
+        else if (input == "3") {
+            return 0;
+        }
+    }
+}
+
+
+
+
+void SingleMode() {
+
+}
+
+void CompareMode() {
+
 }
